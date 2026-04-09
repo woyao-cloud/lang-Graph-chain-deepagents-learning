05dabddfb03f6344643c612ff3018656f47b8045
dcdu <dcdu@cn.ibm.com>
2026-04-09 13:03:14 +0800

    update langGraph quick_start

--- CASE-LangChain/Demo-01/pythonProject/quick_start.py
+++ CASE-LangChain/Demo-01/pythonProject/quick_start.py
@@ -1,10 +1,15 @@
+import os
+
 from langchain.tools import tool
 from langchain.chat_models import init_chat_model
 
+api_key = os.getenv("OPENAI_API_KEY", "sk-723b202be3804cd89fef3970bc92675f")
+api_base = os.getenv("OPENAI_API_BASE", "https://dashscope.aliyuncs.com/compatible-mode/v1")
 
 model = init_chat_model(
-    model="qwen-max",
-    provider="openai",
+    api_key=api_key,
+    base_url=api_base,    # 指向 DashScope 的 OpenAI 兼容 URL
+    model_provider="qwen-max",     # 或 "qwen-turbo" / 您有权限的模型名
     temperature=0,
 )
 
